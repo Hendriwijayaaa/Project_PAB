3fac5204c6deaa2bfb485c30eac25fb61b96026f
Hendri <hendriwijaya_2226240165@mhs.mdp.ac.id>
2024-12-11 17:06:23 +0700

    .

---
+++ wisata_candi/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\u-mdp\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\New folder (4)\\Project_PAB\\wisata_candi" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\u-mdp\\flutter"
+  "PROJECT_DIR=D:\\New folder (4)\\Project_PAB\\wisata_candi"
+  "FLUTTER_ROOT=C:\\Users\\u-mdp\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\New folder (4)\\Project_PAB\\wisata_candi\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\New folder (4)\\Project_PAB\\wisata_candi"
+  "FLUTTER_TARGET=D:\\New folder (4)\\Project_PAB\\wisata_candi\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\New folder (4)\\Project_PAB\\wisata_candi\\.dart_tool\\package_config.json"
+)
